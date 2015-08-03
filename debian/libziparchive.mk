93409c2825d7a5dd127fa01b0f1491792c92c289
Kai-Chung Yan <seamlikok@gmail.com>
2015-08-03 09:52:54 +0800

    d/libziparchive.mk: Add liblog.so linking

--- debian/libziparchive.mk
+++ debian/libziparchive.mk
@@ -6,7 +6,7 @@ OBJECTS = $(SOURCES:.cc=.o)
 CXXFLAGS += -fPIC -c -mno-ms-bitfields
 CPPFLAGS += $(ANDROID_INCLUDES) -I../include -I../../libnativehelper/include/nativehelper
 LDFLAGS += -fPIC -shared -rdynamic -Wl,-rpath=/usr/lib/android \
-           -Wl,-soname,$(NAME).so.5 -lz -L../libutils -lutils
+           -Wl,-soname,$(NAME).so.5 -lz -L../libutils -lutils -L../liblog -llog
 
 build: $(OBJECTS)
 	cc $^ -o $(NAME).so $(LDFLAGS)
